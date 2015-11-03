a811e7beb97ef305f4ce5ecb8ee8dfaa6433ce6a
Derek Fitzsimons <derek.fitzsimons@advancedcomputersoftware.com>
2015-11-03 08:19:08 +0000

    Initial Commit of Netbeans project

---
+++ GitHubDemo/src/githubdemo/GitHubDemo.java
@@ -0,0 +1,21 @@
+/*
+  =================================
+  (C) Derek Fitzsimons 2015
+  =================================
+ */
+package githubdemo;
+
+/**
+ *
+ * @author derek.fitzsimons
+ */
+public class GitHubDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main( String[] args ) {
+        // TODO code application logic here
+    }
+
+}
