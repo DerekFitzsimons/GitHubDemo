5e24a85619c112cb90569c58352f6ed33190ca73
Derek Fitzsimons <derek.fitzsimons@advancedcomputersoftware.com>
2015-11-04 10:51:53 +0000

    Adding files to test refactoring / moving files around

---
+++ GitHubDemo/src/githubdemo/util/Thingumyjig.java
@@ -0,0 +1,14 @@
+/*
+  ==========================================
+  (C) Advanced Computer Software 2000 - 2015
+  ==========================================
+ */
+package githubdemo.util;
+
+/**
+ *
+ * @author derek.fitzsimons
+ */
+public class Thingumyjig {
+
+}
