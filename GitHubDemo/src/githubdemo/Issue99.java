f8cf0ada19136958ab8a4aebd0d0739aea31453c
Derek Fitzsimons <derek.fitzsimons@advancedcomputersoftware.com>
2015-11-09 09:10:18 +0000

    Remove commit direct to master

--- GitHubDemo/src/githubdemo/Issue99.java
+++ GitHubDemo/src/githubdemo/Issue99.java
@@ -7,34 +7,8 @@ package githubdemo;
 
 /**
  *
- * @author N.E. One
+ * @author derek.fitzsimons
  */
 public class Issue99 {
-
-    private String message;
-
-    /**
-     *
-     */
-    public Issue99() {
-        super();
-        message = "áéíóú ÁÉÍÓÚ ¢£¤¥ ¼½¾ ®© ♠♣♥♦ ḉḈ ĉĈ ç æ";
-    }
-
-    /**
-     *
-     * @return
-     */
-    public String getMessage() {
-        return message;
-    }
-
-    /**
-     *
-     * @param message
-     */
-    public void setMessage( String message ) {
-        this.message = message;
-    }
 
 }
