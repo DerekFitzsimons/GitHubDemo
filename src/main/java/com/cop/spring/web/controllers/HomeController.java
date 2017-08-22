8152aeb1f9e4c0f3035e2ea5b3b638336751f6c5
Derek Fitzsimons <dwfitzsimons@gmail.com>
2017-08-22 11:52:03 +0100

    Spring tutorial 122 Using Logging Signed-off-by:Derek Fitzsimons <dwfitzsimons@gmail.com>

--- src/main/java/com/cop/spring/web/controllers/HomeController.java
+++ src/main/java/com/cop/spring/web/controllers/HomeController.java
@@ -5,6 +5,9 @@
  */
 package com.cop.spring.web.controllers;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +18,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HomeController {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger( HomeController.class);
 
     @RequestMapping( "/" )
     public String showHome( Model model ) {
+        
+        LOGGER.info( "Showing home page");
         return "home";
     }
 
